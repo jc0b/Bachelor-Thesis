666071757ad9630c03c7c036b888fdf9ae99864f
jc0b <j@jc0b.computer>
2020-07-11 18:26:33 +0200

    Updated travis build configuration

--- compile.sh
+++ compile.sh
@@ -2,6 +2,6 @@
 echo "Building..."
 mkdir -p ./_build
 latexmk -pdf -outdir=./_build main.tex
-cp _build/main.pdf .
-open main.pdf
-rm -r _build
+#cp _build/main.pdf .
+#open main.pdf
+#rm -r _build
